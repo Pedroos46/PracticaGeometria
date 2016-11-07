a867322c0968ce964019c19112b9525c6ff9d709
aleix26 <aleix66@hotmail.com>
2016-11-07 22:40:29 +0100

    2D + text
    figures 2D amb boto mes click boto figura -> text

--- Geometria/geometria.js
+++ Geometria/geometria.js
@@ -1,14 +1,16 @@
+//var canvas = document.getElementById('tauler');
+//var ctx ;
+
 function dibuixar() {
     var canvas = document.getElementById('tauler');
-    var costats = 99;
-    var D = 2;
-    var p= 1;
+
+
     if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
+      var   ctx = canvas.getContext('2d');
 
         ctx.save();
 
-        if(D == 2){
+
 
         pintarCuadrat(ctx);
         pintarTriangle(ctx);
@@ -19,14 +21,14 @@ function dibuixar() {
         pintarPentagon(ctx);
         pintarHexagon(ctx);
         pintarHeptagon(ctx);
-        pintarTest(ctx);
+        //pintarTest(ctx);
 
 
         }
 
 
 
-    }
+
 }
 
 
@@ -52,6 +54,8 @@ function pintarCuadrat(ctx){
   ctx.strokeStyle="green";
   ctx.rect(100,100,50,50);
   ctx.fill();
+
+
 
 
 }
@@ -203,4 +207,159 @@ function  pintarHexagon(ctx){
 
 
 
-window.addEventListener("load", dibuixar,false);
+
+function clear(){
+
+  var canvas = document.getElementById('tauler');
+//alert("hola");
+
+  if (canvas.getContext) {
+var   ctx = canvas.getContext('2d');
+ctx.clearRect(0,0,2000,2000);
+
+}
+
+}
+
+function text(){
+
+  var canvas = document.getElementById('tauler');
+
+
+  if (canvas.getContext) {
+    var   ctx = canvas.getContext('2d');
+
+switch(this.id){
+          case "Quadrat": textCuadrat(ctx); break;
+          case "Triangle": textTriangle(ctx);break;
+         case "Cercle": textCercle(ctx);break;
+          case "Rectangle": textRectangle(ctx);break;
+          case "Trapezi": textTrapezi(ctx);break;
+          case "Rombe": textRombe(ctx);break;
+          case "Pentagon": textPentagon(ctx);break;
+          case "Hexagon": textHexagon(ctx);break;
+          case "Heptagon": textHeptagon(ctx);break;
+          default: break;
+}
+}
+}
+
+function textCuadrat(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Quadrat: Per = 4 * LCostado ;Area = LCostado^2",180,125);
+
+}
+
+function textTriangle(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Triangle Per = C + C + C; Area = (Base * Costat) / 2",180,300);
+
+}
+
+function textCercle(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Circulo: Per = 2* Pi * Radi; Area = Pi * Radi^2",200,480);
+
+}
+function textRectangle(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Rectangle: Per = 2*base + 2*Altura; Area = Base * Altura",220,620);
+
+}
+function textTrapezi(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Trapezi: Per = Base + BaseSuperior + 2 * Costat; Area = ((Base + BaseSuperior) * Altura) / 2",250,765);
+
+}
+function textRombe(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Rombe: Per = 4 * Costat; Area = Base * AlturaInterior",180,900);
+
+}
+function textPentagon(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Pentagon: Per = 5 * Costat",180,1050);
+
+}
+function textHexagon(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Hexagon: Per = 6 * Costat; Area = (Per * Apotema) /2",180,1200);
+
+}
+function textHeptagon(ctx){
+ctx.font = "12px Arial";
+ctx.fillText("Heptagon: Per = 7 * Costat; Area = (7 * LCostat * Apotema)/2",180,1350);
+
+}
+
+function ferNegatiu(){
+  var canvas = document.getElementById('tauler');
+
+
+  if (canvas.getContext) {
+var   context = canvas.getContext('2d');
+var destX = 0;
+var destY = 0;
+
+var imageData = context.getImageData(0, 210, canvas.width,canvas.height);
+var pixels = imageData.data;
+for (var i = 0; i < pixels.length; i += 4) {
+pixels[i] = 255 - pixels[i]; // red
+pixels[i+1] = 255 - pixels[i+1]; // green
+pixels[i+2] = 255 - pixels[i+2]; // blue
+
+// i+3 es alpha
+  }
+}
+  }
+
+function ferBN(){
+
+  var canvas = document.getElementById('tauler');
+
+
+  if (canvas.getContext) {
+var   context = canvas.getContext('2d');
+var destX = 0;
+var destY = 0;
+
+var imageData = context.getImageData(0, 210, canvas.width,canvas.height);
+var pixels = imageData.data;
+for (var i = 0; i < pixels.length; i += 4) {
+	if(pixels[i]> 127){pixels[i] = 255;}else if(pixels[i]< 127){pixels[i] = 0;}
+	if(pixels[i+1]> 127){pixels[i+1] = 255;}else if(pixels[i+1]< 127){pixels[i+1] = 0;}
+	if(pixels[i+2]> 127){pixels[i+2] = 255;}else if(pixels[i+2]< 127){pixels[i+2] = 0;}
+
+}}
+}
+
+function downloadCanvas() {
+  var canvas = document.getElementById('tauler');
+  var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+
+
+window.location.href=image;
+}
+
+
+document.getElementById("Guardar").addEventListener("click", function() {
+    downloadCanvas();
+}, false);
+//document.getElementById("BN").addEventListener("click", ferBN);
+//document.getElementById("N").addEventListener("click", ferNegatiu);
+document.getElementById("2D").addEventListener("click", dibuixar);
+
+document.getElementById("Quadrat").addEventListener("click", text);
+document.getElementById("Triangle").addEventListener("click", text);
+document.getElementById("Cercle").addEventListener("click", text);
+document.getElementById("Rectangle").addEventListener("click", text);
+document.getElementById("Trapezi").addEventListener("click", text);
+document.getElementById("Rombe").addEventListener("click", text);
+document.getElementById("Pentagon").addEventListener("click", text);
+document.getElementById("Hexagon").addEventListener("click", text);
+document.getElementById("Heptagon").addEventListener("click", text);
+document.getElementById("Cub").addEventListener("click", text);
+document.getElementById("Con").addEventListener("click", text);
+document.getElementById("Cilindre").addEventListener("click", text);
+document.getElementById("Esfera").addEventListener("click", text);
+//window.addEventListener("load", dibuixar,false);
